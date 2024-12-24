3ff3cc2508525b7968aca79746a4e0ee4c96a2db
Milan Godawatta <mgodawat@student.42.fr>
2024-12-24 17:35:38 +0100

    quick update

--- lua/mgodawat/plugins/colorscheme.lua
+++ lua/mgodawat/plugins/colorscheme.lua
@@ -1,27 +1,28 @@
+-- Colorscheme
 return {
-    -- Rose Pine colorscheme
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        priority = 1000,
-        opts = {
-            dark_variant = "main", -- You can change to "moon" or "dawn" if preferred
-            bold_vert_split = false,
-            dim_nc_background = false,
-            disable_background = true, -- Enable transparency
-            disable_float_background = true, -- Floating windows transparent
-            disable_italics = true,
-            highlight_groups = {
-                Function = { fg = "rose", bold = true }, -- Function names bold
-                Identifier = { fg = "pine" }, -- Variables
-                Todo = { fg = "gold", bold = true }, -- TODOs in bold
-                Comment = { fg = "muted", italic = true }, -- Comments italic
-                Error = { fg = "love", bold = true }, -- Errors in bold
-            },
-        },
-        init = function()
-            vim.cmd("colorscheme rose-pine")
-        end,
-    },
+   {
+       "folke/tokyonight.nvim",
+       name = "tokyonight",
+       priority = 1000,
+       opts = {
+           style = "moon",
+           transparent = true,
+           styles = {
+               functions = { bold = true },
+               comments = { italic = true },
+           },
+           on_highlights = function(hl, c)
+               hl.Function = { fg = c.blue, bold = true }
+               hl.Identifier = { fg = c.cyan }
+               hl.Todo = { fg = c.yellow, bold = true }
+               hl.Comment = { fg = c.comment, italic = true }
+               hl.Error = { fg = c.red, bold = true }
+           end
+       },
+       init = function()
+           vim.cmd("colorscheme tokyonight")
+       end,
+   },
 }
+
 
--- lua/mgodawat/plugins/lualine.lua
+++ lua/mgodawat/plugins/lualine.lua
@@ -1,33 +1,33 @@
+-- Lualine
 return {
-    -- Lualine statusline
-    {
-        "nvim-lualine/lualine.nvim",
-        opts = {
-            options = {
-                theme = "rose-pine", -- Use the Rose Pine theme for Lualine
-                section_separators = { left = "", right = "" }, -- Customize separators
-                component_separators = { left = "", right = "" }, -- Customize component separators
-                icons_enabled = true,
-                disabled_filetypes = {}, -- Disable for certain filetypes (if needed)
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {},
-            },
-            extensions = { "quickfix", "fugitive" }, -- Add any specific extensions if needed
-        },
-    },
+   {
+       "nvim-lualine/lualine.nvim",
+       opts = {
+           options = {
+               theme = "tokyonight",
+               section_separators = { left = "", right = "" },
+               component_separators = { left = "", right = "" },
+               icons_enabled = true,
+               disabled_filetypes = {},
+           },
+           sections = {
+               lualine_a = { "mode" },
+               lualine_b = { "branch", "diff", "diagnostics" },
+               lualine_c = { "filename" },
+               lualine_x = { "encoding", "fileformat", "filetype" },
+               lualine_y = { "progress" },
+               lualine_z = { "location" },
+           },
+           inactive_sections = {
+               lualine_a = {},
+               lualine_b = {},
+               lualine_c = { "filename" },
+               lualine_x = { "location" },
+               lualine_y = {},
+               lualine_z = {},
+           },
+           extensions = { "quickfix", "fugitive" },
+       },
+   },
 }
 
