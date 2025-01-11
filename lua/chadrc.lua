d3027b14723b6b98b3b5a29c1e5865ac45b46039
Milan Godawatta <mgodawat@student.42.fr>
2025-01-11 10:42:35 +0100

    transparency update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,12 +6,12 @@
 local M = {}
 
 M.base46 = {
-  theme = "tokyonight",
-
-  hl_override = {
-  Comment = { italic = true },
-  	["@comment"] = { italic = true },
-  },
+    theme = "tokyonight",
+    transparency = true,
+    hl_override = {
+        Comment = { italic = true },
+        ["@comment"] = { italic = true },
+    },
 }
 
 return M
