3246904a7e4b1f4142fac3ca3c431bf3d4606e25
Milan Godawatta <mgodawat@student.42.fr>
2025-01-18 19:43:34 +0100

    quick update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,7 +4,7 @@
 local M = {}
 
 M.base46 = {
-    theme = "onedark",
+    theme = "rosepine",
     transparency = true,
     hl_override = {
         -- Comments and macros (your working part)
@@ -12,17 +12,16 @@ M.base46 = {
         ["@comment"] = { italic = true },
         Define = { italic = true },
         Macro = { italic = true },
-
+        
         -- Function highlights from your screenshot
         Function = { bold = true, italic = false },
         -- ["@keyword"] = { bold = true },
         -- ["@type.builtin"] = { bold = true },
         -- ["@function"] = { bold = true },
         -- ["@function.call"] = { bold = true },
-
-
-         -- Variables in italic
-        -- ["@variable.parameter"] = { italic = true, bold = false },
+        
+        -- Variables in italic
+        ["@variable.parameter"] = { italic = true, bold = false },
     },
 }
 
