86a6e37c847000a447ab2c0934289e52286c7afa
Milan Godawatta <mgodawat@student.42.fr>
2025-01-26 20:10:25 +0100

    nvim update

--- lua/kickstart/plugins/themes.lua
+++ lua/kickstart/plugins/themes.lua
@@ -69,7 +69,7 @@ return {
 				keywordStyle = { italic = true },
 				statementStyle = { bold = true },
 				typeStyle = {},
-				transparent = true, -- do not set background color
+				transparent = false, -- do not set background color
 				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 				terminalColors = true, -- define vim.g.terminal_color_{0,17}
 				colors = { -- add/modify theme and palette colors
@@ -87,7 +87,7 @@ return {
 			})
 
 			-- setup must be called before loading
-			vim.cmd("colorscheme kanagawa")
+			vim.cmd("colorscheme kanagawa-dragon")
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
 		end,
